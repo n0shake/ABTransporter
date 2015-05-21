a0d35502f5683be182a83898c60b4b6bf6c1a457
Abhishek Banthia <abhishekbanthia@me.com>
2015-05-21 12:56:04 +0530

    Removed Error Handling Method
    Need feedback on this before implementing. Also removed unnecessary
    variables and comments.

--- ABMainTransporter.h
+++ ABMainTransporter.h
@@ -11,23 +11,9 @@
 
 @interface ABMainTransporter : NSObject
 
-typedef NS_ENUM(NSInteger, WebServiceResponseStatus)
-{
-    WebServiceFailed = 0,
-    WebServiceSuccess
-};
-
 @property (strong, nonatomic) NSString* executionSelector;
 @property (strong, nonatomic) NSString* interfaceSelector;
 @property (strong, nonatomic) NSString* errorSelector;
-
-/*If selectors are passed as arguments, make sure the sender has implemented them. Otherwise, even though WS will successfully complete, there will be an exception thrown.
- 
- WebService response will be sent as dictionary to the execution selector
- 
- The WS response message will be passed to the UICompletionMethod. Also, the UICompletionMethod will be executed on the main thread.
- 
- Please double check selector names*/
 
 /*The following two methods are created as class methods because:
  
@@ -39,9 +25,6 @@ typedef NS_ENUM(NSInteger, WebServiceResponseStatus)
 + (void)dataWebServicewithWebServicePath:(NSString *)path bySender:(id)sender withCompletionBlock:(void(^)(NSData *data, NSURLResponse *response, NSError *error))completionBlock;
 
 + (void)uploadDataWebServiceWithInputDictionary:(NSDictionary *)inputDictionary andWebServicePath:(NSString *)path bySender:(id)sender withCompletionBlock:(void(^)(NSData *data, NSError *error, NSURLResponse *response))completionBlock;
-
-/*If sender wants to handle error*/
-- (void)errorHandlingUploadWebServicewithInput:(NSDictionary *)inputDictionary andWebServicePath:(NSString *)path bySender:(id)sender;
 
 + (void)setTimeLogger;
 
